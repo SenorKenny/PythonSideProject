6e13d4d9420698bc7d450808b11acaa1e66d0bd2
SenorKenny <elsexylatino@gmail.com>
2026-04-19 13:37:33 -0400

    Made a day 1 exercise testing input, print, and basic variables

---
+++ week_1/day1/Exercise1.py
@@ -0,0 +1,4 @@
+name = input("Whats your name? ")
+age = input("How old are you? ")
+statement = input("tell me something abour yourself ")
+print(name,"is",age,"years old. According to them:",statement)
