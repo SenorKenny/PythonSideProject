16a0f3c96d9cacfe7c732333c505fd212dbd045f
SenorKenny <elsexylatino@gmail.com>
2026-04-28 23:14:55 -0400

    95% finished

--- week_2/Day_15/HereWeGoAgain.py
+++ week_2/Day_15/HereWeGoAgain.py
@@ -7,6 +7,7 @@ import time
 import datetime
 from dotenv import load_dotenv
 import random
+import copy
 
 
 load_dotenv()
@@ -22,6 +23,7 @@ def  url_requester(url,Timeout_val=None,Param_val=None,header_val=None):
         try:
             response=requests.get(url,timeout=Timeout_val,params=Param_val,headers=header_val)
             response.raise_for_status()
+            print("Connection Succesful!")
             return response.json(),response.status_code
         except requests.exceptions.ConnectionError:
             print("Connection error. Check URL")
@@ -29,7 +31,7 @@ def  url_requester(url,Timeout_val=None,Param_val=None,header_val=None):
             print("Connection timeout. Delay your requests")
         except requests.exceptions.Timeout:
             print("Reader timeout. Server may be busy ")
-        except requests.HTTPError as error:
+        except requests.exceptions.HTTPError as error:
             print(f"HTTP error code: {error}")
             return None, response.status_code
         except Exception as othererror:
@@ -62,95 +64,157 @@ def json_parser(json_file):
 #Make Watch+Buy list, compare to a watch/buy list from a json that WE CREATE /already exists
 #If duplicates, do not purchase or put again in buy list.
 #Need to figure out how to remove
-def decision_maker(productlist):
+def decision_maker(productlist,initial_1st,road_map,first_run):
+    flag_basewatch=False
+    flag_liveloop=False
+    _1stwatch=initial_1st
     if productlist is None:
         print("empty input into decisionmaker")
-        return None, None
+        return None, None,None,None,None
     if not isinstance(productlist,list):
         print("Invalid input. List only")
-        return None, None
+        return None, None,None,None,None
     maxwatchprice=float(os.getenv("MAX_WATCH"))
     minwatchprice=float(os.getenv("MIN_WATCH"))
     maxbuyprice=float(os.getenv("MAX_BUY"))
     minbuyprice=float(os.getenv("MIN_BUY"))
-    print(maxbuyprice)
-    print(minbuyprice)
-    print(maxwatchprice)
-    print(minwatchprice)
     comparison_watch_list=[x for x in productlist if maxwatchprice>x.get("price",0)>minwatchprice]
     buy_list=[x for x in productlist if maxbuyprice>x.get("price",0)>minbuyprice]
     # COMPARE.
     # First, confirm there is no json. If not, return our lists. If there i
     #Watch list is the current loops iteration
-    #Goal, make 3 watch lists. first_run, 
-    try:
-        with open("WatchList.json","r") as file:
-            _1stwatch=json.load(file)
-    except FileNotFoundError:
-        _1stwatch=[]
-    except Exception as error:
-        print(f"Other error found: {error}")
-        return
-    if not _1stwatch:
-        _1stwatch=comparison_watch_list
-    else:
+    #Goal, make 3 watch lists. first_run,
+    if first_run:
+        try:
+            with open("WatchList.json") as file:
+                _1stwatch=json.load(file)
+        except FileNotFoundError:
+            _1stwatch=[]
+        except json.JSONDecodeError as error:
+            _1stwatch=[]
+            print(f"Other error found: {error}")
+            return None,None,None,None,None
+        if not _1stwatch:
+            _1stwatch=comparison_watch_list
 
         #comparison_watch_list[1-10].get("title") is not in _1stwatch. add it to _1stwatch
         #same idea with buy list vs bought.
-        if #condition for intialization :
-            road_map={x.get("title"):x.copy() for x in _1stwatch} ## THIS IS THE ORIGINAL AND PERSISTENT. _1stwatch!
+    if first_run:
+            try:
+                with open("Live_watchlist.json") as file:
+                    road_map=json.load(file)
+            except:
+                road_map={x.get("title"):x.copy() for x in _1stwatch} ## THIS IS THE ORIGINAL AND PERSISTENT. _1stwatch!
         
-        for product in comparison_watch_list:
+    for product in comparison_watch_list:
                 name=product.get("title")
                 if name not in road_map:
                     _1stwatch.append(product.copy())
                     road_map[name]=product.copy()
+                    flag_basewatch=True
+                    flag_liveloop=True
                 else: 
                     if product.get("price") < road_map.get(name).get("price"):
                         road_map.get(name)["price"]=product.get("price")
+                        flag_liveloop=True
         #Now we move on to buying decisions
-        for products in _1stwatch:
+    for products in _1stwatch:
             name=products.get("title")
             if name in road_map:
-                if products.get("price")*.75 > road_map[name].get("price"):
-                    buy_list.append(products)
-        return buy_list,_1stwatch,road_map
+                if products.get("price")*.75 > road_map[name].get("price"): #If the updated price is less than
+                    buy_list.append(products)  #75% of the original price upon start up, then we buy.
+                    
+    return buy_list,_1stwatch,road_map,flag_basewatch,flag_liveloop
     
-    def buyer(buy_list,_1stwatch,road_map):
     
     
+def buyer(buy_list,_1stwatch,road_map):
+    flag_basewatch=False
+    flag_liveloop=False
+    flag_purchase=False
     #add checker above for none ig
-
-        for products in buy_list:
+    purchased=[]
+    for products in buy_list:
             name=products.get("title")
             if random.randint(1,6)<= 3:
                 print(f"purchasing {name}")
-                road_map.pop(name)
-                for originals in _1stwatch:
-                    if name in originals.get("title"):
-                        del originals
+                purchased.append(products)
+                flag_purchase=True
+                if name in road_map: 
+                    road_map.pop(name,None) # This will pop the one from road_map. Then remove from first watch without altering object. Add to purchased
+                    _1stwatch[:]=[x for x in _1stwatch if x.get("title") != name]
+                flag_basewatch=True
+                flag_liveloop=True
+            else:
+                print(f"failed to purchase {name}")
+    buy_list[:]=[x for x in buy_list if x not in purchased]
+    return purchased,flag_purchase,flag_basewatch,flag_liveloop
+    
 
+        #improvements. the filtered from decision maker into parser, put buying and decision maker in one function
+def file_saver(_1stwatch,road_map,purchased,dictionary):
         
-                    
-
-
-            
-
-            ## done, i think
-            #next goal, if it is in there, and theres a price change, update price.. could probably add it to
-            #original loop. going to try it the set dictionary.
-
-            #test_set_dictionary={x.get("title") for x in _1stwatch}
+        if dictionary.get("flag_liveloop"):
+            with open("Live_watchlist.json","w") as file:
+                json.dump(road_map,file, indent=4)
         
+        if dictionary.get("flag_basewatch"):
+            with open("Base_watchlist.json","w") as file:
+                json.dump(_1stwatch,file,indent=4)
+        
+        if dictionary.get("flag_purchase"): 
+            with open("Purchases.json","w") as file:
+                json.dump(purchased,file,indent=4)
+        return                    
+def logger(dictionary,purchased,buy_list):
+        timenow=datetime.datetime.now()
+        fails=len(buy_list)-len(purchased)
 
+        with open("Logger_Summary.txt","a") as file:
+            print(timenow,file=file)
+            print(f"{len(purchased)} items purchased, {fails} failed, ", file=file)
+            if dictionary.get("flag_basewatch") or dictionary.get("flag_liveloop"):
+                print("Watchlist changed",file=file)
+def terminaloutput(start_time,purchased,watchlist,i):
+    end_time=datetime.datetime.now()
+    print("="*100)
+    print(f"Script started at {start_time}")
+    print(f"Products purchased: {len(purchased)}")
+    print(f"Number of products in watchlist:{watchlist}")
+    print(f"Number of loops: {i}")
+    print(f"End time: {end_time}")
+    print("="*100)
+    
 
+i=0
+first_run=1
+start_time=datetime.datetime.now()
+_1stwatch=None
+road_map=None
+try:
+    while True:
+        
+        i=i+1
+        print(f"Loop {i}")
+        
+        url=os.getenv("API")
+        converted_request,Status_code=url_requester(url)
+        productlist=json_parser(converted_request)
+        buylist,_1stwatch,road_map,flag_basewatch,flag_liveloop=decision_maker(productlist,_1stwatch,road_map,first_run)
+        purchases,flag_purchase,flag_basewatch2,flag_liveloop2=buyer(buylist,_1stwatch,road_map)
 
-
-
-
-
-url=os.getenv("API")
-converted_request,Status_code=url_requester(url)
-productlist=json_parser(converted_request)
-x,y=decision_maker(productlist)
-
+        if flag_basewatch or flag_basewatch2:
+            flag_basewatch=True
+        if flag_liveloop or flag_liveloop2:
+            flag_liveloop=True
+        booleanDictionary={"flag_basewatch":flag_basewatch,
+                        "flag_liveloop":flag_liveloop,
+                        "flag_purchase":flag_purchase
+        }
+        file_saver(_1stwatch,road_map,purchases,booleanDictionary)
+        logger(booleanDictionary,purchases,buylist)
+        if first_run == 1:
+            first_run=0
+        time.sleep(10)
+except KeyboardInterrupt:
+    terminaloutput(start_time,purchases,_1stwatch,i)
--- week_2/Day_15/tester.py
+++ week_2/Day_15/tester.py
@@ -1,47 +1,12 @@
+list1 =[1,2]
 
+list2 =[2,1]
 
-list_1=  [{
-    "title":"cats",
-    "name":"sasha",
-    "color":"grey",
-    "weight":10
-},{
-    "title":"cats",
-    "name":"tab",
-    "color":"orange",
-    "weight":15
-},{
-    "title":"cats",
-    "name":"coco",
-    "color":"red",
-    "weight":6
-}]
+if list1[0] in list2:
+    print("true")
+else:
+    print("false")
 
-list_2=[{
-    "title":"cats",
-    "name":"sasha",
-    "color":"grey",
-    "weight":13
-},{
-    "title":"cats",
-    "name":"Kiwi",
-    "color":"grey",
-    "weight":9
-}]
-####
-
-##====================================================== trying set dicitionary
-
-set_list={old.get("name") for old in list_1}
-print(set_list)
-
-for names in list_2:
-    if names.get("name") not in set_list:
-        list_1.append(names)
-    else:
-        addition=[names.keys]
-
-print(addition)
         
 
 
